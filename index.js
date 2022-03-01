9343dcd1789c052573908dac36d35d1ef56464f9
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-01 22:21:24 +0300

    fix code review

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'G-ES0LN8KC8W');
+
 function toggleMenu (menu){
 document.getElementById("toggle-menu").classList.toggle("toggle-menu")
 document.querySelector("header").classList.toggle("remove-padding")
-// menu.classList.toggle("open")
 }

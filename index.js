beb00a4a394dcc5e06692701993d3bafd2f637de
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-01 20:52:47 +0300

    Mobile Menu

---
+++ index.js
@@ -0,0 +1,5 @@
+function toggleMenu (menu){
+document.getElementById("toggle-menu").classList.toggle("toggle-menu")
+document.querySelector("header").classList.toggle("remove-padding")
+// menu.classList.toggle("open")
+}

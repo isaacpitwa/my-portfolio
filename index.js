331e675bd332d122e54c7d6ae0dbdad33fb343a8
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-01 23:03:55 +0300

    Fix default behaviour on anchor

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ function toggleMenu() {
   document.getElementById('toggle-menu').classList.toggle('toggle-menu');
   document.querySelector('header').classList.toggle('remove-padding');
 }
+function mobileMenuOption(event, id) {
+  event.preventDefault();
+  toggleMenu();
+  const linkTo = document.getElementById(id);
+  linkTo.scrollIntoView();
+}

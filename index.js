59e35cc2535b9e5a85cff5d810e116bc40ada44c
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-02 03:58:38 +0300

    Dynamic popup data

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const projects = [
     role: 'Back End Dev',
     year: '2015',
     live_link: 'https://github.com/isaacpitwa/my-portfolio',
+    github_link: 'https://github.com/isaacpitwa/my-portfolio',
   },
   {
     id: 2,
@@ -37,7 +38,8 @@ const projects = [
     client: 'CANOPY',
     role: 'Back End Dev',
     year: '2015',
-
+    live_link: 'https://github.com/isaacpitwa/my-portfolio',
+    github_link: 'https://github.com/isaacpitwa/my-portfolio',
   },
   {
     id: 4,
@@ -48,7 +50,8 @@ const projects = [
     client: 'CANOPY',
     role: 'Back End Dev',
     year: '2015',
-
+    live_link: 'https://github.com/isaacpitwa/my-portfolio',
+    github_link: 'https://github.com/isaacpitwa/my-portfolio',
   },
   {
     id: 5,
@@ -59,14 +62,15 @@ const projects = [
     client: 'CANOPY',
     role: 'Back End Dev',
     year: '2015',
-
+    live_link: 'https://github.com/isaacpitwa/my-portfolio',
+    github_link: 'https://github.com/isaacpitwa/my-portfolio',
   },
 ];
 
 window.onload = (params) => {
   const workSection = document.getElementById('Portoflio');
   projects.forEach((project) => {
-    const projectHTML = ` <div class="work-card">
+    const projectHTML = ` <div class="work-card"  id="${project.id}">
                                 <img src="${project.featured_image}" alt="${project.name}" />
                                 <div class="content">
                                     <h2 class="project-title">${project.name}</h2>
@@ -81,7 +85,7 @@ window.onload = (params) => {
                                     <ul class="languages">
                                         ${project.technologies.map((tech) => `<li class="lang">${tech}</li>`).join('')}
                                     </ul>
-                                    <button class="details-btn" type="button">See Project</button>
+                                    <button class="details-btn" type="button" onclick ="showProject(${project.id})">See Project</button>
                                 </div>
                             </div>`;
     workSection.insertAdjacentHTML('beforeend', projectHTML);
@@ -97,4 +101,55 @@ function mobileMenuOption(event, id) {
   toggleMenu();
   const linkTo = document.getElementById(id);
   linkTo.scrollIntoView();
+}
+function showProject(id) {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+  const body = document.querySelector('body');
+  const project = projects.find((project) => project.id === id);
+  const popup = ` <section class="project-details">
+  <div class="details-card">
+      <div class="heading">
+          <div>
+              <h2 class="project-title">${project.name}</h2>
+              <div class="stats">
+                  <h3 class="client">${project.client}</h3>
+                  <div class="dot"></div>
+                  <h3 class="myrole">${project.role}</h3>
+                  <div class="dot"></div>
+                  <h3 class="year">${project.year}</h3>
+              </div>
+          </div>
+          <img src="images/icons/close_gray.svg" alt="Close" onclick="closeProjectDetails()" />
+
+      </div>
+      <img src="${project.featured_image}" alt="${project.name}" class="project-image"/>
+      <div class="project-desc">
+          <p>${project.description}</p>
+          <div class="details-actions">
+              <ul class="languages">
+              ${project.technologies.map((tech) => `<li class="lang">${tech}</li>`).join('')}
+              </ul>
+              <ul class="languages languages-extra">
+                  <li class="lang">github</li>
+                  <li class="lang">ruby</li>
+                  <li class="lang">Bootstrap</li>
+              </ul>
+              <hr/>
+              <div class="project-btns">
+                  <button type="button" >See live <span><img src="images/icons/external_link.svg" alt="See Live"></span></button>
+                  <button type="button" >See  Source <span><img src="images/icons/github_link.svg" alt="See Source"></span></button>
+              </div>
+          </div>
+      </div>
+  </div>
+</section>`;
+  body.insertAdjacentHTML('beforebegin', popup);
+}
+
+function closeProjectDetails() {
+  const openPopupCards = document.getElementsByClassName('project-details');
+  for (let index = 0; index < openPopupCards.length; index += 1) {
+    const element = openPopupCards[index];
+    element.parentNode.removeChild(element);
+  }
 }
